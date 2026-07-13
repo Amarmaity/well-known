f279179f6bff3f10396c3f787814a567e5505396
Amar <amarmaity243@gmail.com>
2026-07-13 10:35:56 +0530

    change the welcome mail evaluation_cradentials

--- resources/views/emails/evaluation_credentials.blade.php
+++ resources/views/emails/evaluation_credentials.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Evaluation Credentials</title>
 </head>
+
 <body>
-    <h2>Hello {{ $user->fname }} {{$user->lname}}</h2>
+    <h2>Hello {{ $user->fname }} {{ $user->lname }}</h2>
 
     <p>You have been granted access for {{ config('app.name') }}.</p>
 
@@ -17,11 +19,19 @@
     <p>You can log in using the link below:</p>
     <p><a href="{{ $loginUrl }}">{{ $loginUrl }}</a></p>
 
-    {{-- <p>If you have any issues, feel free to contact our team.</p> --}}
+    <p style="color:#d9534f; font-weight:bold;">
+        🔒 Security Notice
+    </p>
+
+    <p>
+        For security reasons, we strongly recommend changing your password
+        immediately after your first login. Please keep your login credentials
+        confidential and do not share them with anyone.
+    </p>
 
     <br>
     <p>Regards,<br>
-    {{-- {{ config('app.name') }} --}}
-    Delostyle Team</p>
+        Delostyle Team</p>
 </body>
+
 </html>
